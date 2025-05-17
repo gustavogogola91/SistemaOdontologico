5784adf81a3b6097e71df61bae1cff3fc89b1c76
Jujej <jpdurau@gmail.com>
2025-05-17 08:16:09 -0300

    Colocado em comentário a linha //app.UseHttpsRedirection(); do program.cs

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -7,7 +7,6 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Dentista> Dentistas { get; set; } // Adicione esta linha
-
     public DbSet<Dentista> tb_dentista { get; set; }
     public DbSet<Consulta> tb_consulta { get; set; } // Adicione esta linha
     public DbSet<Paciente> tb_paciente { get; set; } // Adicione esta linha
