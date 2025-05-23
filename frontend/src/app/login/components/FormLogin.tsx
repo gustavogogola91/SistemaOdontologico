024accdb59606fd386b6dc64750d9e189106571d
joaopauloclass <theofficialspam8@gmail.com>
2025-05-23 18:50:41 -0300

    JWT- Frontend implementation

--- frontend/src/app/login/components/FormLogin.tsx
+++ frontend/src/app/login/components/FormLogin.tsx
@@ -1,33 +1,73 @@
 "use client"
 
+import { AuthContext, SignInData } from "@/app/contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import { parseCookies } from "nookies";
+import { useContext, useEffect, useState } from "react";
+
 
 export const FormLogin = () => {
+
+     const router = useRouter();
+
+    const { 'auth-token': AuthToken } = parseCookies();
+
+    // useEffect(() => {
+    //     if (typeof window !== 'undefined') {
+
+    //         if (AuthToken) {
+    //             router.push('/Pages/AulasPage');
+    //         }
+    //     }
+    // }, [router]);
+
+
+    const { loginUsuario } = useContext(AuthContext);
+
+    const [loginData, setLoginData] = useState({
+        username: "",
+        senha: ""
+    });
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setLoginData({ ...loginData, [name]: value });
+    };
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
-        // const loginRequestData: SignInData = {
-        //     email: loginData.email,
-        //     senha: loginData.senha
+        const loginRequestData: SignInData = {
+            username: loginData.username,
+            senha: loginData.senha
+        }
+
+        const success = await loginUsuario(loginRequestData)
+
+        if (success) {
+            console.log(localStorage.getItem('AuthToken'));
+
+            // router.push('/Pages/AulasPage');
+        } else {
+            alert("Email ou senha inválidos!");
+        }
+
+        // var usuario = await loginUsuario(loginData.email, loginData.senha);
+        // if (usuario !== null) {
+        //     alert("Parabens, você lembrou seu login (aqui n temos opção de recuperação, Guarde bem)!");
+        //     // Redirecionar para a página inicial ou outra página desejada
+        //     return;
         // }
 
-        // const success = await loginUsuario(loginRequestData)
-
-        // if (success) {
-        //     console.log(localStorage.getItem('AuthToken'));
-
-        //     router.push('/Pages/AulasPage');
-        // } else {
-        //     alert("Email ou senha inválidos!");
-        // }
-    }
+    };
     return(
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username" className="block">Usuário</label>
-            <input type="text" name="username" id="username" />
-            <label htmlFor="password" className="block">Senha</label>
-            <input type="password" name="password" id="password" />
-            <button type="submit">Entrar</button>
-        </form>
+         <>
+            <form className="base-form flex flex-col w-full max-w-xs gap-4" onSubmit={handleSubmit}>
+                <input name="username" type="text" placeholder="Username" className="" value={loginData.username} onChange={handleChange} required />
+                <input name="senha" type="password" placeholder="Senha" className="" value={loginData.senha} onChange={handleChange} required />
+                <button type="submit" className="btn-primary py-2 text-[16px] font-bold"> Entrar </button>
+            </form>
+        </>
     )
 }
