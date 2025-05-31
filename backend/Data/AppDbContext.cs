7c95a2d67da4d5a4d49a7ec87a69eae3156b2454
gustavogogola91 <gustavogogola3@gmail.com>
2025-05-30 21:38:52 -0300

    alterado nome da tabela

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<Dentista> Dentistas { get; set; } // Adicione esta linha
+    public DbSet<Dentista> tb_dentista { get; set; } // Adicione esta linha
     public DbSet<Consulta> tb_consulta { get; set; } // Adicione esta linha
     public DbSet<Paciente> tb_paciente { get; set; } // Adicione esta linha
     public DbSet<Procedimento> tb_procedimento { get; set; } // Adicione esta linha
